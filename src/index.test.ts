909a3197df747c746594bcf53007e1a45c936b53
Elian Cordoba <elian@whyline.com>
2019-01-08 10:47:33 +0000

    Add simple array test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,13 @@
 import forEachSafe from './index';
 
-test('Should iterate through all levels', () => {
-  const simpleData = { a: 1, b: 2, c: { d: 3, e: { f: 4 } }, g: 5 };
+const simpleObject = { a: 1, b: 2, c: { d: 3, e: { f: 4 } }, g: 5 };
+const simpleArray = [ 1, 2, 'a', null, {}, [] ];
 
+test('Should iterate through all levels of an object', () => {
   let keys = [] as any;
   let values = [] as any;
   
-  forEachSafe(simpleData, (value, key) => {
+  forEachSafe(simpleObject, (value, key) => {
     keys.push(key);
 
     // Prevent the nodes itself from going into the array @TODO Add option to disable this
@@ -16,4 +17,12 @@ test('Should iterate through all levels', () => {
   });
   expect(keys).toEqual([ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]);
   expect(values).toEqual([ 1, 2, 3, 4, 5 ]);
+});
+
+test('Should iterate through all elements in an array', () => {
+  let items = [] as any;
+
+  forEachSafe(simpleArray, item => items.push(item));
+
+  expect(items).toEqual([ 1, 2, 'a', null, {}, [] ])
 });
